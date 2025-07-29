eff3377a3e8263ad83778dc9e85edc9536e59367
Ravikiran09309 <kummariravikiran982@gmail.com>
2025-07-29 19:06:06 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDom from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 ReactDom.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
